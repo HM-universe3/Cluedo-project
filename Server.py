cb86e47756e0ba5fe79b336eb4fa81bab035f14e
HM-universe3 <110479905+HM-universe3@users.noreply.github.com>
2024-03-03 18:06:02 +0000

    Another attempt to connect the network and server

--- Server.py
+++ Server.py
@@ -3,55 +3,58 @@ from _thread import *
 import threading
 import sys
 import pickle
+from Network import Network
 
 HOST = "192.168.0.50"
 PORT = 55555
 
 clients = [] #to hold which clients are connected
 
-def threaded_client(conn): #runs in background
-    conn.send(str.encode("Connected"))
-    
-    while True:
-        try:
-            #this receives the data from the client
-            data = conn.recv(2048) #larger the size, longer it takes to recieve 
+class Server:
 
-            if not data:
-                print("Disconnected")
-                break
+    def threaded_client(conn): #runs in background
+        conn.send(str.encode("Connected"))
+    
+        while True:
+            try:
+                #this receives the data from the client
+                data = conn.recv(2048) #larger the size, longer it takes to recieve 
+
+                if not data:
+                    print("Disconnected")
+                    break
             
-            for client in clients:
-                if client != conn:
-                    try:
-                        client.send(data)
-                    except:
-                        clients.remove(client)
-        except:
-            break
+                for client in clients:
+                    if client != conn:
+                        try:
+                            client.send(data)
+                        except:
+                            clients.remove(client)
+            except:
+                break
     
-    clients.remove(conn)
-    print("Lost connection")
-    conn.close()
+        clients.remove(conn)
+        print("Lost connection")
+        conn.close()
 
 
-def server():
-    socketForServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socketForServer.bind((HOST, PORT))
-    socketForServer.listen()
+    def server():
+        socketForServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socketForServer.bind((HOST, PORT))
+        socketForServer.listen()
 
-    while True: #continuously listens for connections
-        conn, addr = socketForServer.accept(conn, addr) #accepts incoming connections
-        clients.append(conn)
-        print("Connected to:", addr)
+        while True: #continuously listens for connections
+            conn, addr = socketForServer.accept(conn, addr) #accepts incoming connections
+            clients.append(conn)
+            print("Connected to:", addr)
 
-        #need to start a new thread for each client that's connected
-        clientHandler = threading.Thread(target=threaded_client, args=(conn,))
-        clientHandler.start()
+            #need to start a new thread for each client that's connected
+            clientHandler = threading.Thread(target=threaded_client, args=(conn,))
+            clientHandler.start()
 
 
-if __name__ == "__main__":
-    server()
+    if __name__ == "__main__":
+        server()
 
 class Client:
     def __init__(self):
