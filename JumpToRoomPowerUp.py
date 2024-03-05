087efd4fd1d3c2c1ae875352b8575c8541941d47
HM-universe3 <110479905+HM-universe3@users.noreply.github.com>
2024-03-05 21:19:30 +0000

    i hate this project

--- JumpToRoomPowerUp.py
+++ JumpToRoomPowerUp.py
@@ -199,51 +199,52 @@ class Fountain:
     
 
 
-run = True
-while run:
-  WIN.fill((202, 228, 241))
-  WIN.blit(cluedoBoard, (0,0))
+def main():
+  run = True
+  while run:
+    WIN.fill((202, 228, 241))
+    WIN.blit(cluedoBoard, (0,0))
 
-  if garage_button.draw() == True:
-    GarageDisplay = Garage()
-    GarageDisplay.run()
+    if garage_button.draw() == True:
+      GarageDisplay = Garage()
+      GarageDisplay.run()
 
-  if dining_button.draw() == True:
-    DiningDisplay = DiningRoom()
-    DiningDisplay.run()
+    if dining_button.draw() == True:
+      DiningDisplay = DiningRoom()
+      DiningDisplay.run()
 
-  if bath_button.draw() == True:
-    BathDisplay = Bathroom()
-    BathDisplay.run()
+    if bath_button.draw() == True:
+      BathDisplay = Bathroom()
+      BathDisplay.run()
 
-  if bed_button.draw() == True:
-    BedDisplay = Bedroom()
-    BedDisplay.run()
+    if bed_button.draw() == True:
+      BedDisplay = Bedroom()
+      BedDisplay.run()
 
-  if greenH_button.draw() == True:
-    GreenDisplay = GreenHouse()
-    GreenDisplay.run()
+    if greenH_button.draw() == True:
+      GreenDisplay = GreenHouse()
+      GreenDisplay.run()
 
-  if living_button.draw() == True:
-    livingDisplay = LivingRoom()
-    livingDisplay.run()
+    if living_button.draw() == True:
+      livingDisplay = LivingRoom()
+      livingDisplay.run()
 
-  if office_button.draw() == True:
-    officeDisplay = Office()
-    officeDisplay.run()
+    if office_button.draw() == True:
+      officeDisplay = Office()
+      officeDisplay.run()
 
-  if hall_button.draw() == True:
-    hallDisplay = Hallway()
-    hallDisplay.run()
+    if hall_button.draw() == True:
+      hallDisplay = Hallway()
+      hallDisplay.run()
 
-  if fountain_button.draw() == True:
-    fountainDisplay = Fountain()
-    fountainDisplay.run()
+    if fountain_button.draw() == True:
+      fountainDisplay = Fountain()
+      fountainDisplay.run()
 
-for event in pygame.event.get():
-  if event.type == pygame.QUIT:
-    run = False
-pygame.display.update()
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+  pygame.display.update()
 
-
+main()
 pygame.quit()
